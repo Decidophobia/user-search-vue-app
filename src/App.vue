c239b8f20645102c0db9357bc3bc7ef46b734015
Tanya <tearfulta@gmail.com>
2021-03-15 00:55:54 +0300

    app split into search & userlist components, minor styling

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,23 @@
 <template>
   <div id="app">
-    <div class="search-panel">
-      <button
-        @click="
-          changeOrder();
-          onSubmit();
-        "
-      >
-        Order: {{ this.order }}
-      </button>
-      <br />
-      <input type="text" @keydown.enter="onSubmit()" v-model="inputValue" />
-
-      <div class="users-list">
-        <div v-for="user in usersPrev" :key="user.id" class="users-list-_item">
-          {{ user.login }}
-        </div>
-      </div>
+    <Search/>
+    <user-list/>
     </div>
- 
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+
 import Search from './components/Search.vue';
-
-// import UserList from './components/UserList'
-
+import UserList from './components/UserList.vue';
 
 export default {
   name: 'App',
   components: {
-    Search
-   
+    Search,
+    UserList,
   },
-  data() {
-    return {
-      inputValue: '',
-      users: [],
-      usersPrev: [],
-      //search params
-      sortType: 'repositories',
-      order: 'desc',
-      itemsPerPage: 10,
-      pageNumber: 1,
-    };
-  },
-
-  methods: {
-    ...mapActions('users', ['fetchGetUsers']),
-    async onSubmit() {
-      this.users = await this.fetchGetUsers({
-        name: this.inputValue,
-        sortType: this.sortType,
-        order: this.order,
-        itemsPerPage: this.itemsPerPage,
-        pageNumber: this.pageNumber,
-      });
-
-      this.usersPrev = this.users.map((user) => {
-        return {
-          login: user.login,
-          id: user.id,
-        };
-      });
-    },
-    changeOrder() {
-      this.order = this.order === 'desc' ? 'asc' : 'desc';
-    },
-  },
+  
 };
 </script>
 
@@ -78,8 +26,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 600px;
+  margin: 0 auto;
 }
 </style>
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,9 +1,55 @@
 <template>
-  <div>asfnafnafa</div>
+  <div class="users-list">
+    <div v-for="user in usersPrev" :key="user.id" class="users-list_item">
+      <div class="users-list_image">
+        <img :src="user.avatar" :alt="user.login" />
+      </div>
+      <div class="users-list_login">
+        <div class="users-list_login-label">
+          Пользователь:
+        </div>
+        <div class="users-list_login-text">
+          {{ user.login }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export const UserList = {
-  name: 'UserList'
-}
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'UserList',
+  computed: {
+    ...mapGetters({
+      usersPrev: 'users/getUsersPrev',
+    }),
+  },
+};
 </script>
+
+<style scoped>
+.users-list_item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 14px;
+  margin-top: 36px;
+}
+.users-list_item:hover {
+  box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.7);
+  transform: scale(1.02);
+  border-radius: 8px;
+}
+.users-list_image,
+img {
+  width: 100px;
+  border-radius: 50%;
+  
+}
+.users-list_login {
+  flex-grow: 1;
+  margin-left: 16px;
+}
+</style>

73814080bcf848fc8c33b617b3e92240bd5dc118
Tanya <tearfulta@gmail.com>
2021-03-15 04:39:10 +0300

    show more info on click, but for all users

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="users-list">
     <div v-for="user in usersPrev" :key="user.id" class="users-list_item">
+      <div class="user-list_prev" @click="showMoreInfo()">
       <div class="users-list_image">
         <img :src="user.avatar" :alt="user.login" />
       </div>
@@ -11,7 +12,16 @@
         <div class="users-list_login-text">
           {{ user.login }}
         </div>
+        </div>
       </div>
+        <div class="users-list_full-info" v-show="isClicked">
+          <div class="users-list_full-info_label">
+            Перейти на профиль
+          </div>
+          <div class="users-list_full-info_text">
+            <a :href="user.gitHub">gitHub</a>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -21,15 +31,29 @@ import { mapGetters } from 'vuex';
 
 export default {
   name: 'UserList',
+  data(){
+    return{
+      isClicked: false,
+      id: ''
+    }
+  },
   computed: {
     ...mapGetters({
       usersPrev: 'users/getUsersPrev',
     }),
   },
+  methods: {
+    showMoreInfo(){
+      this.isClicked = !this.isClicked
+      console.log(this.isClicked);
+    }
+  },
+
 };
 </script>
 
 <style scoped>
+
 .users-list_item {
   display: flex;
   justify-content: space-between;
@@ -42,6 +66,11 @@ export default {
   transform: scale(1.02);
   border-radius: 8px;
 }
+.user-list_prev{
+  display: flex;
+    flex-grow: 1;
+    align-items: center;
+}
 .users-list_image,
 img {
   width: 100px;
@@ -52,4 +81,5 @@ img {
   flex-grow: 1;
   margin-left: 16px;
 }
+
 </style>
